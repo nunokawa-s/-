47f2a1dec5b94f227283a85eda1116d1225547b0
nunokawa-s <momenn54@gmail.com>
2025-05-13 20:00:26 +0900

    Delete src/main directory

--- src/main/java/com/example/NAGOYAMESHI/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.NAGOYAMESHI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.NAGOYAMESHI.entity.VerificationToken;
-
-public interface VerificationTokenRepository extends JpaRepository< VerificationToken, Integer> {
-	    public VerificationToken findByToken(String token);
-}
